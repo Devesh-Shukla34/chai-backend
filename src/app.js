d7164d8db64951b5f3a3c4ba1544136457437055
Devesh-Shukla34 <dshukla034.btech2023@me.nitrr.ac.in>
2024-08-01 00:10:08 +0530

    all setup configure

--- src/app.js
+++ src/app.js
@@ -12,4 +12,13 @@ app.use(express.urlencoded({extended:true,limit:"20kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+//routes
+
+import userRouter from './routes/user.routes.js'
+
+//router declaration
+
+app.use("/api/v1/users",userRouter)
+
+//https://localhost:8000/api/v1/users/register
 export{app}
